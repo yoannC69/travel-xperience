9bd6967cf909897195bccee4bacfaf951f4dd431
Yoann <yo.cote69@gmail.com>
2021-06-13 22:11:17 +0200

    authentification system

---
+++ api/middleware/auth.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const decodedToken = jwt.verify(token, "RANDOM_TOKEN_SECRET");
+    const idUser = decodedToken.idUser;
+    if (req.body.idUser && req.body.idUser !== idUser) {
+      throw "idUser invalide";
+    } else {
+      next();
+    }
+  } catch {
+    res.status(401).json({
+      error: new Error("Vous n'êtes pas autorisé à faire cette action"),
+    });
+  }
+};
--- api/routes/reservation.routes.js
+++ api/routes/reservation.routes.js
@@ -1,28 +1,29 @@
 module.exports = app => {
     const reservations = require("../controllers/reservation.controller.js");
+    const auth = require("../middleware/auth");
   
     var router = require("express").Router();
   
     // Create a new reservation
-    router.post("/", reservations.create);
+    router.post("/",auth, reservations.create);
   
     // Retrieve all reservations
-    router.get("/", reservations.findAll);
+    router.get("/",auth, reservations.findAll);
   
     // Retrieve all published reservations
-    router.get("/date", reservations.findAllByDate);
+    router.get("/date",auth, reservations.findAllByDate);
   
     // Retrieve a single reservation with id
-    router.get("/:id", reservations.findOne);
+    router.get("/:id",auth, reservations.findOne);
   
     // Update a reservation with id
-    router.put("/:id", reservations.update);
+    router.put("/:id",auth, reservations.update);
   
     // Delete a reservation with id
-    router.delete("/:id", reservations.delete);
+    router.delete("/:id",auth, reservations.delete);
   
     // Delete all reservations
-    router.delete("/", reservations.deleteAll);
+    router.delete("/",auth, reservations.deleteAll);
 
     app.use('/reservations', router);
   };
